f9af7df33f31cdac03713f86d3f2fad55c8cd099
twweal <twealit@gmail.com>
2024-07-24 09:47:10 +0300

    Added comment

--- app/src/main/java/com/example/application_1/MainActivity.kt
+++ app/src/main/java/com/example/application_1/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//CCCOOOOOOOMMMMMMMMMMMEEEEEEENNNNTTTTTTTTTTTTTTTTTTTT
+//CCCOOOOOOOMMMMMMMMMMMEEEEEEENNNNTTTTTTTTTTTTTTTTTTTTT
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
